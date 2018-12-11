ab14020706f9cc582803535ad4f5a387917aa76f
Honkasalo Antti <s1800074@student.businesscollege.fi>
2018-12-11 15:24:26 +0200

    Initial commit

---
+++ src/Gameover.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './gameover.css';
+import './app.css';
+
+class Gameover extends Component {
+  render() {
+    const score = this.props.scoreCallback();
+
+    return(
+      <div id="overlay">
+        <div id="gameover">Your score: <br/>{score}
+        <button id="startgame" onClick={ this.props.startCallback }>New Game</button></div>
+      </div>
+    );
+
+  }
+}
+
+export default Gameover;
